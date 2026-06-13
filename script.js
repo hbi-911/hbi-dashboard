03e0956961e2b6143d02e1a6b6ceee29e7f04663
hbi-911 <veritasconsultancy2020@gmail.com>
2026-06-13 19:25:14 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
+function getRisk(heatIndex) {
+
+    if (heatIndex >= 54)
+        return "Extreme Danger";
+
+    if (heatIndex >= 41)
+        return "Danger";
+
+    if (heatIndex >= 32)
+        return "Extreme Caution";
+
+    return "Caution";
+}
+
 async function loadHeatData() {
 
     const response = await fetch("heat_data.json");
 
     const data = await response.json();
 
-    const tableBody = document.getElementById("heat-table-body");
+    const tableBody =
+        document.getElementById("heat-table-body");
+
+    const topCity =
+        document.getElementById("top-city");
 
     tableBody.innerHTML = "";
 
-    data.forEach((city, index) => {
+    const sorted =
+        data.sort((a, b) =>
+            b.heat_index - a.heat_index);
 
-        const row = document.createElement("tr");
+    const highest = sorted[0];
 
-        row.innerHTML = `
+    topCity.innerHTML =
+        `
+        ${highest.city}<br>
+        ${highest.heat_index}°C<br>
+        ${getRisk(highest.heat_index)}
+        `;
+
+    sorted.forEach((city, index) => {
+
+        const row =
+            document.createElement("tr");
+
+        row.innerHTML =
+            `
             <td>${index + 1}</td>
             <td>${city.city}</td>
             <td>${city.heat_index}°C</td>
-        `;
+            <td>${getRisk(city.heat_index)}</td>
+            `;
 
         tableBody.appendChild(row);
 
